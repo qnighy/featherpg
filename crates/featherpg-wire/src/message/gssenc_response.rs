cd6759a8e503f0812d099904c0ba412758ad9d8a
Masaki Hara <ackie.h.gmai@gmail.com>
2026-01-03 18:01:53 +0900

    Add more parser impl

--- crates/featherpg-wire/src/message/gssenc_response.rs
+++ crates/featherpg-wire/src/message/gssenc_response.rs
@@ -16,23 +16,34 @@ pub enum GSSENCResponse {
     ErrorResponse(ErrorResponse),
 }
 
+impl From<UseGSSENC> for GSSENCResponse {
+    fn from(msg: UseGSSENC) -> Self {
+        GSSENCResponse::UseGSSENC(msg)
+    }
+}
+
+impl From<NoGSSENC> for GSSENCResponse {
+    fn from(msg: NoGSSENC) -> Self {
+        GSSENCResponse::NoGSSENC(msg)
+    }
+}
+
+impl From<ErrorResponse> for GSSENCResponse {
+    fn from(msg: ErrorResponse) -> Self {
+        GSSENCResponse::ErrorResponse(msg)
+    }
+}
+
 impl GSSENCResponse {
     pub fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
     where
-        W: Write,
+        W: Write + ?Sized,
     {
         match self {
-            GSSENCResponse::UseGSSENC(msg) => {
-                msg.write_to(writer)?;
-            }
-            GSSENCResponse::NoGSSENC(msg) => {
-                msg.write_to(writer)?;
-            }
-            GSSENCResponse::ErrorResponse(err) => {
-                err.write_to(writer)?;
-            }
+            GSSENCResponse::UseGSSENC(msg) => msg.write_to(writer),
+            GSSENCResponse::NoGSSENC(msg) => msg.write_to(writer),
+            GSSENCResponse::ErrorResponse(err) => err.write_to(writer),
         }
-        Ok(())
     }
 
     pub fn read_from<R>(reader: &mut R) -> IoResult<Self>
@@ -43,11 +54,10 @@ impl GSSENCResponse {
         reader.read_exact(slice::from_mut(&mut type_byte))?;
 
         match type_byte {
-            UseGSSENC::TYPE_BYTE => Ok(GSSENCResponse::UseGSSENC(UseGSSENC)),
-            NoGSSENC::TYPE_BYTE => Ok(GSSENCResponse::NoGSSENC(NoGSSENC)),
+            UseGSSENC::TYPE_BYTE => UseGSSENC::read_after_type_byte(reader, type_byte).map(Into::into),
+            NoGSSENC::TYPE_BYTE => NoGSSENC::read_after_type_byte(reader, type_byte).map(Into::into),
             ErrorResponse::TYPE_BYTE => {
-                let error = ErrorResponse::read_after_type_byte(reader, type_byte)?;
-                Ok(GSSENCResponse::ErrorResponse(error))
+                ErrorResponse::read_after_type_byte(reader, type_byte).map(Into::into)
             }
             _ => Err(WireFormatError::GSSENCResponseUnknownTypeByte { type_byte }.into()),
         }
@@ -64,12 +74,21 @@ pub struct UseGSSENC;
 impl UseGSSENC {
     pub const TYPE_BYTE: u8 = b'G';
 
-    pub fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
+    fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
     where
         W: Write + ?Sized,
     {
         writer.write_u8(Self::TYPE_BYTE)?;
         Ok(())
+    }
+
+    fn read_after_type_byte<R>(_reader: &mut R, type_byte: u8) -> IoResult<Self>
+    where
+        R: GetReadBuf + ?Sized,
+    {
+        assert_eq!(type_byte, Self::TYPE_BYTE);
+
+        Ok(UseGSSENC)
     }
 }
 
@@ -82,11 +101,128 @@ pub struct NoGSSENC;
 impl NoGSSENC {
     pub const TYPE_BYTE: u8 = b'N';
 
-    pub fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
+    fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
     where
         W: Write + ?Sized,
     {
         writer.write_u8(Self::TYPE_BYTE)?;
         Ok(())
+    }
+
+    fn read_after_type_byte<R>(_reader: &mut R, type_byte: u8) -> IoResult<Self>
+    where
+        R: GetReadBuf + ?Sized,
+    {
+        assert_eq!(type_byte, Self::TYPE_BYTE);
+
+        Ok(NoGSSENC)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::CStr;
+
+    use crate::errors::{DiagnosticMessage, DiagnosticSeverity};
+
+    use super::*;
+
+    fn to_bytes(msg: &GSSENCResponse) -> IoResult<Vec<u8>> {
+        let mut buf = Vec::new();
+        msg.write_to(&mut buf)?;
+        Ok(buf)
+    }
+
+    fn from_bytes(data: &[u8]) -> IoResult<GSSENCResponse> {
+        let mut reader = data;
+        GSSENCResponse::read_from(&mut reader)
+    }
+
+    #[test]
+    fn test_use_gssenc_writing_packet() {
+        let msg = GSSENCResponse::UseGSSENC(UseGSSENC);
+        assert_eq!(to_bytes(&msg).unwrap(), b"G");
+    }
+
+    #[test]
+    fn test_use_gssenc_parsing_packet() {
+        let msg = from_bytes(b"G").unwrap();
+        assert_eq!(msg, GSSENCResponse::UseGSSENC(UseGSSENC));
+    }
+
+    #[test]
+    fn test_no_gssenc_writing_packet() {
+        let msg = GSSENCResponse::NoGSSENC(NoGSSENC);
+        assert_eq!(to_bytes(&msg).unwrap(), b"N");
+    }
+
+    #[test]
+    fn test_no_gssenc_parsing_packet() {
+        let msg = from_bytes(b"N").unwrap();
+        assert_eq!(msg, GSSENCResponse::NoGSSENC(NoGSSENC));
+    }
+
+    #[test]
+    fn test_error_response_writing_packet() {
+        let err = DiagnosticMessage {
+            severity: DiagnosticSeverity::Error,
+            localized_severity: CStr::from_bytes_with_nul(b"ERROR\0").unwrap().to_owned(),
+            code: CStr::from_bytes_with_nul(b"28000\0").unwrap().to_owned(),
+            message: CStr::from_bytes_with_nul(b"authentication failed\0")
+                .unwrap()
+                .to_owned(),
+            detail: None,
+            hint: None,
+            position: None,
+            internal_position: None,
+            internal_query: None,
+            where_: None,
+            schema_name: None,
+            table_name: None,
+            column_name: None,
+            data_type_name: None,
+            constraint_name: None,
+            file: None,
+            line: None,
+            routine: None,
+        };
+        let msg = GSSENCResponse::ErrorResponse(ErrorResponse { error: err });
+        assert_eq!(
+            to_bytes(&msg).unwrap(),
+            b"E\x00\x00\x00\x31SERROR\0VERROR\0C28000\0Mauthentication failed\0\0"
+        );
+    }
+
+    #[test]
+    fn test_error_response_parsing_packet() {
+        let msg =
+            from_bytes(b"E\x00\x00\x00\x31SERROR\0VERROR\0C28000\0Mauthentication failed\0\0")
+                .unwrap();
+        let expected_err = DiagnosticMessage {
+            severity: DiagnosticSeverity::Error,
+            localized_severity: CStr::from_bytes_with_nul(b"ERROR\0").unwrap().to_owned(),
+            code: CStr::from_bytes_with_nul(b"28000\0").unwrap().to_owned(),
+            message: CStr::from_bytes_with_nul(b"authentication failed\0")
+                .unwrap()
+                .to_owned(),
+            detail: None,
+            hint: None,
+            position: None,
+            internal_position: None,
+            internal_query: None,
+            where_: None,
+            schema_name: None,
+            table_name: None,
+            column_name: None,
+            data_type_name: None,
+            constraint_name: None,
+            file: None,
+            line: None,
+            routine: None,
+        };
+        let expected_msg = GSSENCResponse::ErrorResponse(ErrorResponse {
+            error: expected_err,
+        });
+        assert_eq!(msg, expected_msg);
     }
 }
--- crates/featherpg-wire/src/message/ssl_response.rs
+++ crates/featherpg-wire/src/message/ssl_response.rs
@@ -16,23 +16,34 @@ pub enum SSLResponse {
     ErrorResponse(ErrorResponse),
 }
 
+impl From<UseSSL> for SSLResponse {
+    fn from(msg: UseSSL) -> Self {
+        SSLResponse::UseSSL(msg)
+    }
+}
+
+impl From<NoSSL> for SSLResponse {
+    fn from(msg: NoSSL) -> Self {
+        SSLResponse::NoSSL(msg)
+    }
+}
+
+impl From<ErrorResponse> for SSLResponse {
+    fn from(msg: ErrorResponse) -> Self {
+        SSLResponse::ErrorResponse(msg)
+    }
+}
+
 impl SSLResponse {
     pub fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
     where
         W: Write + ?Sized,
     {
         match self {
-            SSLResponse::UseSSL(msg) => {
-                msg.write_to(writer)?;
-            }
-            SSLResponse::NoSSL(msg) => {
-                msg.write_to(writer)?;
-            }
-            SSLResponse::ErrorResponse(err) => {
-                err.write_to(writer)?;
-            }
+            SSLResponse::UseSSL(msg) => msg.write_to(writer),
+            SSLResponse::NoSSL(msg) => msg.write_to(writer),
+            SSLResponse::ErrorResponse(err) => err.write_to(writer),
         }
-        Ok(())
     }
 
     pub fn read_from<R>(reader: &mut R) -> IoResult<Self>
@@ -43,11 +54,10 @@ impl SSLResponse {
         reader.read_exact(slice::from_mut(&mut type_byte))?;
 
         match type_byte {
-            UseSSL::TYPE_BYTE => Ok(SSLResponse::UseSSL(UseSSL)),
-            NoSSL::TYPE_BYTE => Ok(SSLResponse::NoSSL(NoSSL)),
+            UseSSL::TYPE_BYTE => UseSSL::read_after_type_byte(reader, type_byte).map(Into::into),
+            NoSSL::TYPE_BYTE => NoSSL::read_after_type_byte(reader, type_byte).map(Into::into),
             ErrorResponse::TYPE_BYTE => {
-                let error = ErrorResponse::read_after_type_byte(reader, type_byte)?;
-                Ok(SSLResponse::ErrorResponse(error))
+                ErrorResponse::read_after_type_byte(reader, type_byte).map(Into::into)
             }
             _ => Err(WireFormatError::SSLResponseUnknownTypeByte { type_byte }.into()),
         }
@@ -64,12 +74,21 @@ pub struct UseSSL;
 impl UseSSL {
     pub const TYPE_BYTE: u8 = b'S';
 
-    pub fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
+    fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
     where
         W: Write + ?Sized,
     {
         writer.write_u8(Self::TYPE_BYTE)?;
         Ok(())
+    }
+
+    fn read_after_type_byte<R>(_reader: &mut R, type_byte: u8) -> IoResult<Self>
+    where
+        R: GetReadBuf + ?Sized,
+    {
+        assert_eq!(type_byte, Self::TYPE_BYTE);
+
+        Ok(UseSSL)
     }
 }
 
@@ -82,11 +101,128 @@ pub struct NoSSL;
 impl NoSSL {
     pub const TYPE_BYTE: u8 = b'N';
 
-    pub fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
+    fn write_to<W>(&self, writer: &mut W) -> IoResult<()>
     where
         W: Write + ?Sized,
     {
         writer.write_u8(Self::TYPE_BYTE)?;
         Ok(())
+    }
+
+    fn read_after_type_byte<R>(_reader: &mut R, type_byte: u8) -> IoResult<Self>
+    where
+        R: GetReadBuf + ?Sized,
+    {
+        assert_eq!(type_byte, Self::TYPE_BYTE);
+
+        Ok(NoSSL)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::CStr;
+
+    use crate::errors::{DiagnosticMessage, DiagnosticSeverity};
+
+    use super::*;
+
+    fn to_bytes(msg: &SSLResponse) -> IoResult<Vec<u8>> {
+        let mut buf = Vec::new();
+        msg.write_to(&mut buf)?;
+        Ok(buf)
+    }
+
+    fn from_bytes(data: &[u8]) -> IoResult<SSLResponse> {
+        let mut reader = data;
+        SSLResponse::read_from(&mut reader)
+    }
+
+    #[test]
+    fn test_use_ssl_writing_packet() {
+        let msg = SSLResponse::UseSSL(UseSSL);
+        assert_eq!(to_bytes(&msg).unwrap(), b"S");
+    }
+
+    #[test]
+    fn test_use_ssl_parsing_packet() {
+        let msg = from_bytes(b"S").unwrap();
+        assert_eq!(msg, SSLResponse::UseSSL(UseSSL));
+    }
+
+    #[test]
+    fn test_no_ssl_writing_packet() {
+        let msg = SSLResponse::NoSSL(NoSSL);
+        assert_eq!(to_bytes(&msg).unwrap(), b"N");
+    }
+
+    #[test]
+    fn test_no_ssl_parsing_packet() {
+        let msg = from_bytes(b"N").unwrap();
+        assert_eq!(msg, SSLResponse::NoSSL(NoSSL));
+    }
+
+    #[test]
+    fn test_error_response_writing_packet() {
+        let err = DiagnosticMessage {
+            severity: DiagnosticSeverity::Error,
+            localized_severity: CStr::from_bytes_with_nul(b"ERROR\0").unwrap().to_owned(),
+            code: CStr::from_bytes_with_nul(b"28000\0").unwrap().to_owned(),
+            message: CStr::from_bytes_with_nul(b"authentication failed\0")
+                .unwrap()
+                .to_owned(),
+            detail: None,
+            hint: None,
+            position: None,
+            internal_position: None,
+            internal_query: None,
+            where_: None,
+            schema_name: None,
+            table_name: None,
+            column_name: None,
+            data_type_name: None,
+            constraint_name: None,
+            file: None,
+            line: None,
+            routine: None,
+        };
+        let msg = SSLResponse::ErrorResponse(ErrorResponse { error: err });
+        assert_eq!(
+            to_bytes(&msg).unwrap(),
+            b"E\x00\x00\x00\x31SERROR\0VERROR\0C28000\0Mauthentication failed\0\0"
+        );
+    }
+
+    #[test]
+    fn test_error_response_parsing_packet() {
+        let msg =
+            from_bytes(b"E\x00\x00\x00\x31SERROR\0VERROR\0C28000\0Mauthentication failed\0\0")
+                .unwrap();
+        let expected_err = DiagnosticMessage {
+            severity: DiagnosticSeverity::Error,
+            localized_severity: CStr::from_bytes_with_nul(b"ERROR\0").unwrap().to_owned(),
+            code: CStr::from_bytes_with_nul(b"28000\0").unwrap().to_owned(),
+            message: CStr::from_bytes_with_nul(b"authentication failed\0")
+                .unwrap()
+                .to_owned(),
+            detail: None,
+            hint: None,
+            position: None,
+            internal_position: None,
+            internal_query: None,
+            where_: None,
+            schema_name: None,
+            table_name: None,
+            column_name: None,
+            data_type_name: None,
+            constraint_name: None,
+            file: None,
+            line: None,
+            routine: None,
+        };
+        let expected_msg = SSLResponse::ErrorResponse(ErrorResponse {
+            error: expected_err,
+        });
+        assert_eq!(msg, expected_msg);
     }
 }
